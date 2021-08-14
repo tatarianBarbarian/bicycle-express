fe9ac71fda3788179f62ce327ba2c2083fa9744a
Феликс Хафизов <f.hafizov@reg.ru>
2021-08-14 23:14:01 +0300

    Create Request class

--- src/main.js
+++ src/main.js
@@ -2,23 +2,43 @@ import http from 'http';
 import { Handler } from './Handler/Handler.js';
 import { get, getRouteHandler, matchRouteForUrl } from './Route/Route.js';
 
-// TODO: Препроцессинг req.url, чтобы убрать квери и сложить их в обжект
+class Request {
+    raw = null;
+    params = {};
+    query = {};
+    method = 'GET';
 
-const middleware = new Handler((req, res, next) => { next() });
+    constructor(raw) {
+        const url = new URL(raw.url, `http://${raw.headers.host}`);
+        // this.raw = raw;
+        this.url = url.pathname;
+        this.query = Object.fromEntries(url.searchParams.entries());
+        this.method = raw.method;
+        console.log(this);
+    }
+}
+
+const processUrlMiddleware = new Handler((req, res, next) => {
+    next();
+});
+const middleware = processUrlMiddleware;
 
 function listen(port) {
     const server = http.createServer((request, response) => {
+        const req = new Request(request);
+
         response.writeHead(200, { 
             'Content-Type': 'text/plain',
             'Trailer': 'Content-MD5' 
         });        
-        const { method, url } = request;
+        const { method, url } = req;
+
+        middleware.handle(req, response);
+
         const routeHandler = getRouteHandler(matchRouteForUrl(method, url));
 
-        middleware.handle(request, response);
-
         if (routeHandler) {
-            routeHandler.handle(request, response);
+            routeHandler.handle(req, response);
         }
 
         response.end();
