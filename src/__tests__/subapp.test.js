8194b3ea8e5383859ccfc50b8aa57378b3619aad
Феликс Хафизов <f.hafizov@reg.ru>
2021-08-16 10:26:53 +0300

    Tests for sub-sub apps

--- src/__tests__/subapp.test.js
+++ src/__tests__/subapp.test.js
@@ -3,9 +3,11 @@ import { press } from '../main.js';
 
 const app = press();
 const subApp = press();
+const subSubApp = press();
 const subAppMount = '/hello';
+const subSubAppMount = '/alternative'
 
-const map = {
+const subappMap = {
     root: {
         url: '/',
         respond: 'Subapp root\n'
@@ -20,45 +22,109 @@ const map = {
     }
 };
 
-subApp.get(map.root.url, (req, res) => {
-    res.write(map.root.respond);
+const subSubappMap = {
+    root: {
+        url: '/',
+        respond: 'Sub-sub-app root\n'
+    },
+    fr: {
+        url: '/fr',
+        respond: 'Bonjoir!\n'
+    },
+    jp: {
+        url: '/jp',
+        respond: 'Konichiwa!\n'
+    }
+};
+
+subApp.get(subappMap.root.url, (req, res) => {
+    res.write(subappMap.root.respond);
 });
 
-subApp.get(map.ru.url, (req, res) => {
-    res.write(map.ru.respond);
+subApp.get(subappMap.ru.url, (req, res) => {
+    res.write(subappMap.ru.respond);
 });
 
-subApp.get(map.en.url, (req, res) => {
-    res.write(map.en.respond);
+subApp.get(subappMap.en.url, (req, res) => {
+    res.write(subappMap.en.respond);
 });
 
+subSubApp.get(subSubappMap.root.url, (req, res) => {
+    res.write(subSubappMap.root.respond);
+});
+
+subSubApp.get(subSubappMap.fr.url, (req, res) => {
+    res.write(subSubappMap.fr.respond);
+});
+
+subSubApp.get(subSubappMap.jp.url, (req, res) => {
+    res.write(subSubappMap.jp.respond);
+});
+
+subApp.use(subSubAppMount, subSubApp);
 app.use(subAppMount, subApp);
 
 describe('Subapps', () => {
+    const getUrl = (path) => subAppMount + subappMap[path].url;
+    const getRespond = (path) => subappMap[path].respond;
+
     test('Subapp correctly responds to root request', (done) => {
         request(app.server) // FIXME: leaky thing
-        .get(subAppMount + map.root.url)
+        .get(getUrl('root'))
         .then(response => {
             expect(response.statusCode).toBe(200);
-            expect(response.text).toEqual(map.root.respond);
+            expect(response.text).toEqual(getRespond('root'));
             done();
         });
     });
 
     test('Subapp correctly responds to routes', (done) => {
         request(app.server) // FIXME: leaky thing
-        .get(subAppMount +  map.ru.url)
+        .get(getUrl('ru'))
         .then(response => {
             expect(response.statusCode).toBe(200);
-            expect(response.text).toEqual(map.ru.respond);
+            expect(response.text).toEqual(getRespond('ru'));
             done();
         });
 
         request(app.server) // FIXME: leaky thing
-        .get(subAppMount + map.en.url)
+        .get(getUrl('en'))
         .then(response => {
             expect(response.statusCode).toBe(200);
-            expect(response.text).toEqual(map.en.respond);
+            expect(response.text).toEqual(getRespond('en'));
+            done();
+        });
+    });
+});
+
+describe('Sub-sub-apps', () => {
+    const getUrl = (path) => subAppMount + subSubAppMount + subSubappMap[path].url;
+    const getRespond = (path) => subSubappMap[path].respond;
+
+    test('Sub-sub correctly responds to root request', (done) => {
+        request(app.server) // FIXME: leaky thing
+        .get(getUrl('root'))
+        .then(response => {
+            expect(response.statusCode).toBe(200);
+            expect(response.text).toEqual(getRespond('root'));
+            done();
+        });
+    });
+
+    test('Sub-sub correctly responds to routes', (done) => {
+        request(app.server) // FIXME: leaky thing
+        .get(getUrl('fr'))
+        .then(response => {
+            expect(response.statusCode).toBe(200);
+            expect(response.text).toEqual(getRespond('fr'));
+            done();
+        });
+
+        request(app.server) // FIXME: leaky thing
+        .get(getUrl('jp'))
+        .then(response => {
+            expect(response.statusCode).toBe(200);
+            expect(response.text).toEqual(getRespond('jp'));
             done();
         });
     });
